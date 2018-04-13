e3d14ba933715ca4b931248c8a9c6cdfc28f0d9e
Kostygin <kostygin.den@gmail.com>
2018-04-13 13:25:00 +0700

    Added enum for direction and Entity class for further changes

---
+++ src/pacman/api/Direction.java
@@ -0,0 +1,3 @@
+package pacman.api;
+
+public enum Direction {LEFT, RIGHT, UP, DOWN, NOTING}
---
+++ src/pacman/api/Entity.java
@@ -0,0 +1,8 @@
+package pacman.api;
+
+public class Entity {
+    public int x;
+    public int y;
+    public Direction direction;
+    public Direction nextDirection;
+}
