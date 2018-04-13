299cf8e54aaa5aee894b0309b0fc0680927d509c
Kostygin <kostygin.den@gmail.com>
2018-04-13 14:01:52 +0700

    Changed main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 import pacman.api.IPacManAPI;
 import pacman.api.PacManAPI;
+import pacman.bot.PacManSearchBotV2;
 import pacman.bot.PacManRandomBot;
-import pacman.bot.PacManSearchBot;
 import pacman.client.BotViewWindow;
 import pacman.client.PeopleViewWindow;
+import pacman.client.SettingGameWindow;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        //randomBot(PacManAPI.GameType.VERSUS);
+        //randomBot(PacManAPI.GameType.VERSUS_WITH_GHOST);
         //searchBot(PacManAPI.GameType.SINGLE);
-        people(PacManAPI.GameType.SINGLE);
+        //searchBotV2(PacManAPI.GameType.VERSUS_WITH_GHOST);
+        //people(PacManAPI.GameType.VERSUS_WITHOUT_GHOST);
+        new SettingGameWindow();
     }
 
     private static void randomBot(PacManAPI.GameType gameType) {
@@ -20,11 +23,11 @@ public class Main {
         }
     }
 
-    private static void searchBot(PacManAPI.GameType gameType) {
+    private static void searchBotV2(PacManAPI.GameType gameType) {
         IPacManAPI api = new PacManAPI();
         if (api.connection("127.0.0.1", 7070, gameType)) {
             new BotViewWindow(api, gameType);
-            new PacManSearchBot(api).start();
+            new PacManSearchBotV2(api).start();
         }
     }
 
