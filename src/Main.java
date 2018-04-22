ea5540e287deb5d97c223f1b0330565334566f1d
Rozhkov Maxim <rozhkovm96@yandex.ru>
2018-04-23 02:59:57 +0700

    Fix doc

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import pacman.api.GameInfo;
+import pacman.api.IPacManAPI;
+import pacman.api.PacManAPI;
 import pacman.client.SettingGameWindow;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        new SettingGameWindow();
+        IPacManAPI api = new PacManAPI();
+        if (api.connection("127.0.0.1", 7070,
+                PacManAPI.GameType.SINGLE_WITHOUT_GHOST)) {
+            while (true) {
+                GameInfo gameInfo = api.getInfoAboutLeftPlayer();
+                if (gameInfo.pacMan.score != 500) {
+                    api.toDown();
+                } else {
+                    api.disconnect(false);
+                    System.exit(0);
+                }
+            }
+        }
     }
 }
